db2a8adc130daf73ef2155cfc6b919367d16cb4f
anurag <anuragraushan373@gmail.com>
2023-03-01 11:52:35 +0530

    project form front end

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,28 @@ import Users from './pages/Users';
 import Sidebar from './components/Sidebar';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import ProjectForm from './pages/ProjectForm';
 
 function App() {
   return (
     <>
       <Router>
-        <Sidebar>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/fileManager" element={<FileManager />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/saved" element={<Saved />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="*" element={<>Not Found</>} />
-          </Routes>
-        </Sidebar>
+        {/* <Sidebar> */}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/fileManager" element={<FileManager />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/form/modify" element={<ProjectForm />} />
+          <Route path="*" element={<>Not Found</>} />
+        </Routes>
+        {/* </Sidebar> */}
       </Router>
     </>
   );
